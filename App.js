4694b185abc299bd4b3c8be8f9720d9096d854a1
thierrymgn <maignan.thierry@outlook.fr>
2023-10-23 22:24:50 +0200

    add login screen base code

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import ChatScreen from './pages/ChatScreen'
+import ChatScreen from './screens/ChatScreen'
 
 export default function App() {
   return (
