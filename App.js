a3742a2270e0b08be613a6965fb1952798c63fa8
Cammyxalot <renoir.fab@outlook.com>
2023-10-24 10:36:45 +0200

    add registerScreen and navigation

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import ChatScreen from './screens/ChatScreen'
+import StackNavigator from './StackNavigator'
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <StatusBar style="auto" />
-      <ChatScreen />
-    </View>
+    <>
+      <StackNavigator />
+    </>
   )
 }
 
