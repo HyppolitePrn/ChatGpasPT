267ce3e1350869ea3c2cf241b05cb08c4757ce7a
thierrymgn <maignan.thierry@outlook.fr>
2023-11-12 21:03:57 +0100

    Add socket.io for real-time communication

--- api/app.js
+++ api/app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const cors = require('@fastify/cors')
 const routes = require('./routes');
 
+const io = require('socket.io')(fastify.server);
+
 mongoose.connect(
     `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.1ix928u.mongodb.net/`,
     {
@@ -25,9 +27,22 @@ routes.forEach((route) => {
     fastify.route(route);
 })
 
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected');
+//     });
+
+//     socket.on('message', (msg) => {
+//         console.log('message: ' + msg);
+//         socket.broadcast.emit('message', msg);
+//     });
+// });
+
 try {
-    fastify.listen({port: process.env.DB_PORT, host: process.env.PRIVATE_IP})
-    
+    fastify.listen({ port: process.env.DB_PORT, host: process.env.PRIVATE_IP })
+
 } catch (error) {
     fastify.log.error(err);
 }
