a01dafe9e6aeedd06d1f95b73bb4c2509f8eeb5d
thierrymgn <maignan.thierry@outlook.fr>
2023-11-18 18:40:54 +0100

    update auth middleware to white list login and register

--- api/middlewares/authMiddleware.js
+++ api/middlewares/authMiddleware.js
@@ -1,11 +1,21 @@
+// middlewares/authMiddleware.js
+
 const jwt = require('jsonwebtoken')
 
 module.exports = async function (fastify) {
+  // Add a preHandler to every route
   fastify.addHook('preHandler', async (request, reply) => {
+    const whitelist = ['/login', '/register']
+
+    // If the route is in the whitelist, skip token verification
+    if (whitelist.includes(request.routerPath)) {
+      return
+    }
+
     try {
       const authHeader = request.headers.authorization
       if (!authHeader) {
-        throw new Error('No token provided')
+        throw new Error('Authentication failed. Please provide a token.')
       }
 
       const token = authHeader.split(' ')[1]
@@ -13,7 +23,7 @@ module.exports = async function (fastify) {
 
       request.user = { id: decoded.id }
     } catch (err) {
-      reply.send(err)
+      reply.code(401).send({ message: err.message })
     }
   })
 }
